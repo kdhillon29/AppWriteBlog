7db41a9d2af9bd80f9db4d9d7904546b8b090564
kanwar Dhillon <kdhillon469@gmail.com>
2024-01-28 12:20:44 +1100

    Header & footer component added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="bg-gray-300 w-full py-2 px-1 text-right space-x-1 ">
+      <p className="text-light text-blue-500">
+        <strong className="  mr-10"> My Blog</strong>@Kanwar Dhillon
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
