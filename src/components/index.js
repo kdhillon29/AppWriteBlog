947f866770374c4dfd5c38853b0c4c70c197a74a
kanwar Dhillon <kdhillon469@gmail.com>
2024-01-29 17:03:45 +1100

    All Pages added

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,16 @@ import Footer from "./Footer/Footer"
 import Container from "./contanier/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogOut"
+import Input from "./Input"
+import RTE from "./RTE"
+import Signup from "./SignUp"
+import Button from "./Button"
+import Select from "./Select"
+import Login from "./Login"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
-export { Header, Footer, Container, Logo, LogoutBtn }
+export {
+    Header, Footer, Container, Logo, LogoutBtn, Input, RTE, Signup, Select,
+    Button, Login, PostCard, AuthLayout
+}
